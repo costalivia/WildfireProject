46131cae85493f3e858059bb47cd81c69cf86589
ZoeZhouJ <jiarurzhou@bren.ucsb.edu>
2024-03-21 23:31:14 -0700

    added data documentation

---
+++ R/wildfire_data.R
@@ -0,0 +1,17 @@
+#' Wildfire Risk Assessment Data for Santa Barbara
+#'
+#' This dataset contains estimated data considering the most recent minimum and maximum temperatures,
+#' humidity, and precipitation in Santa Barbara for 2023.
+#'
+#' @format A data frame with 15 rows and 4 variables
+#' \describe{
+#'   \item{year}{Year of the data collection.}
+#'   \item{t}{Mean temperature (°C).}
+#'   \item{p}{Mean precipitation (mm).}
+#'   \item{h}{Mean humidity (%).}
+#' }
+#' @source Estimates based on real data available at different internet sources.
+"wildfire_data"
+NULL
+#' @export
+
