639e9a81305830ed70132855cf5b22d13e3a1791
ZoeZhouJ <jiarurzhou@bren.ucsb.edu>
2024-03-21 21:17:31 -0700

    documentation

---
+++ data/wildfire_data.R
@@ -0,0 +1,36 @@
+#' Wildfire dataset from 2009 to 2023
+#'
+#' This dataset provides weather indices including mean temperature, mean precipitation,
+#' and mean humidity for Santa Barbara County from 2009 to 2023.
+#'
+#' @name wildfire_data
+#' @description
+#' This dataset contains annual mean temperature, mean precipitation, and mean humidity
+#' for Santa Barbara County from 2009 to 2023.
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{Year}{Year of observation}
+#'   \item{Mean_Temperature}{Mean temperature in Celsius}
+#'   \item{Mean_Precipitation}{Mean precipitation in mm}
+#'   \item{Mean_Humidity}{Mean humidity in percentage}
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' data(wildfire_data)
+#' }
+#'
+#' @source The values of temperature (T), precipitation (p), and humidity (h) are estimated based
+#' on real weather data of Santa Barbara County for the year 2023.
+#'
+#' @param T Annual mean temperature (Celsius) from 2009 to 2023
+#' @param p Annual mean precipitation (mm) from 2009 to 2023
+#' @param h Annual mean humidity (%) from 2009 to 2023
+#' @author Livia Costa and Zoe Zhou
+#' @export
+
+library(roxygen2)
+roxygen2::roxygenize()
+devtools::document()
+
